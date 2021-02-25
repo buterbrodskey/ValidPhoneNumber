bd6f86383032cdb7566dc58f89a4edfea0aee0d5
Andrey <stantonfeng@yandex.ru>
2021-02-25 14:18:34 +0300

    fix

--- src/com/mypackage/Main.java
+++ src/com/mypackage/Main.java
@@ -3,22 +3,23 @@ package com.mypackage;
 public class Main {
 
     public static void main(String[] args) {
-        String[] message;
-        String[] numberSet = {"",
+        String message[];
+        String numberSet[] = {"",
                 "89001112233",
                 "+790011122333",
                 "890011122331",
                 "8 900 111 22 33",
                 "8(900)1112233",
                 "8(900)1112233",
-                "8(900) 111 22 33 2"
-                ,"+7 900 333 (22 11)"
-                ,null
+                "8(900) 111 22 33 2",
+                "+7 900 333 (22 11)",
+                "+7 900 (22 11)",
+                null
         };
         for (int i = 0; i < numberSet.length; i++) {
             message = isValidPN(numberSet[i]);
 
-            System.out.println(message[1] + " " + message[0]);
+            System.out.println(message[0] + "\n    " + message[1]);
         }
 
     }
@@ -29,8 +30,7 @@ public class Main {
         if (number == null) return resultMessage;
 
         if (number.contains("+7")) {
-            number = number.replaceFirst("7","8");
-            number = number.replace("+","");
+            number = number.replace("+7","8");
             resultMessage[1] += "Заменено +7 на 8; ";
         }
 
@@ -44,13 +44,16 @@ public class Main {
             number = number.replace(")","");
             resultMessage[1] += "В номере есть скобки; ";
         }
+
+        resultMessage[0] = number;
+
         if (number.length() != 11) {
             resultMessage[1] += "Количество символов не равно 11; ";
+            resultMessage[0] = "Введён некорректный номер";
         }
         if (resultMessage[1].isEmpty()) {
             resultMessage[1] += "Изменений не было";
         }
-        resultMessage[0] = number;
         return resultMessage;
     }
 }
